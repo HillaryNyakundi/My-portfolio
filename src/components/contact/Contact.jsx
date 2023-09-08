d8058596b2b4ae6042cc7b7c71c3aef8b46054c6
Hillary Nyakundi <hillarynyakundi66@gmail.com>
2023-09-08 16:36:09 +0300

    contact x

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './contact.css' 
 import {MdOutlineEmail} from 'react-icons/md'
-import {FaTwitter} from 'react-icons/fa'
+import {RiTwitterXFill} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
 import { useRef } from 'react';
 import emailjs from 'emailjs-com'
@@ -23,7 +23,7 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className=" contact__option">
-            <FaTwitter className='contact__option-icon'/>
+            <RiTwitterXFill className='contact__option-icon'/>
             <h4>X.corp</h4>
             {/* <h5>@bcryptidd</h5> */}
             <a href="https://twitter.com/bcryptidd" target="_blank">Send a message</a>
