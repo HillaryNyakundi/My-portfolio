744a0eb3b655f0111568218fab69e0eb780b967a
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-03-17 09:05:25 +0300

    made design changes on the form placeholder text

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import './about.css'
-import MEEP from '../../assets/meep.jpg'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {BsFolderSymlinkFill} from 'react-icons/bs'
+import React from "react";
+import "./about.css";
+import MEEP from "../../assets/meep.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { BsFolderSymlinkFill } from "react-icons/bs";
 
 const about = () => {
   return (
@@ -11,43 +11,47 @@ const about = () => {
       <h5>Get To Know</h5>
       <h2>About me</h2>
 
-      <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src={MEEP} alt="About Image" />
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={MEEP} alt='About Image' />
           </div>
         </div>
 
-        <div className="about__content">
-          <div className="about__cards">
+        <div className='about__content'>
+          <div className='about__cards'>
             <article className='about__card'>
-              <FaAward className='about__icon'/>
+              <FaAward className='about__icon' />
               <h5>Experience</h5>
-              <small>1+ Years working</small>
+              <small>0-1 Years working</small>
             </article>
-            
+
             <article className='about__card'>
-              <FiUsers className='about__icon'/>
+              <FiUsers className='about__icon' />
               <h5>Clients</h5>
               <small>1+ Nairobi</small>
             </article>
-            
+
             <article className='about__card'>
-              <BsFolderSymlinkFill className='about__icon'/>
+              <BsFolderSymlinkFill className='about__icon' />
               <h5>Projects</h5>
-              <small>1+ Completed</small>
+              <small>6+ Completed</small>
             </article>
           </div>
 
           <p>
-            My name is Hillary Nyakundi, a P/MERN software engineer. I'm passinate about bringing value and benefit in people's lives through software creation by solving real life problems.
+            My name is Hillary Nyakundi, a P/MERN software engineer. I'm
+            passinate about bringing value and benefit in people's lives through
+            software creation by solving real life problems.
           </p>
-          
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+          <a href='#contact' className='btn btn-primary'>
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default about
+export default about;
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import './contact.css' 
-import {MdOutlineEmail} from 'react-icons/md'
-import {FaTwitter} from 'react-icons/fa'
-import {BsWhatsapp} from 'react-icons/bs'
-import { useRef } from 'react';
-import emailjs from 'emailjs-com'
+import React from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaTwitter } from "react-icons/fa";
+import { BsWhatsapp } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_ic0glmi', 'template_44qu0g9', form.current, 'DsJ5grcCywXRQl9AZ')
-    e.target.reset()
-
+    emailjs.sendForm(
+      "service_ic0glmi",
+      "template_44qu0g9",
+      form.current,
+      "DsJ5grcCywXRQl9AZ"
+    );
+    e.target.reset();
   };
 
   return (
@@ -20,39 +24,57 @@ const Contact = () => {
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
 
-      <div className="container contact__container">
-        <div className="contact__options">
-          <article className=" contact__option">
-            <FaTwitter className='contact__option-icon'/>
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className=' contact__option'>
+            <FaTwitter className='contact__option-icon' />
             <h4>X.corp</h4>
             {/* <h5>@bcryptidd</h5> */}
-            <a href="https://twitter.com/nyakundi66" target="_blank">Send a message</a>
+            <a href='https://twitter.com/nyakundi66' target='_blank'>
+              Send a message
+            </a>
           </article>
-          
-          <article className=" contact__option">
-            <MdOutlineEmail className='contact__option-icon'/>
+
+          <article className=' contact__option'>
+            <MdOutlineEmail className='contact__option-icon' />
             <h4></h4>
             <h5>Mail me</h5>
-            <a href="mailto:hillarynyakundi66@gmail.com" target='_blank'>Send a message</a>
+            <a href='mailto:hillarynyakundi66@gmail.com' target='_blank'>
+              Send a message
+            </a>
           </article>
-          
-          <article className=" contact__option">
-            <BsWhatsapp className='contact__option-icon'/>
+
+          <article className=' contact__option'>
+            <BsWhatsapp className='contact__option-icon' />
             <h4>Text</h4>
             <h5></h5>
-            <a href="https://wa.me/+254741467228" target="_blank">Send a message</a>
+            <a href='https://wa.me/+254741467228' target='_blank'>
+              Send a message
+            </a>
           </article>
         </div>
         {/* END OF CONTACT OPTION */}
-        <form  ref={form} onSubmit={sendEmail} method ="post">
-          <input type="text" name='name' placeholder='Your Full Name' required/>
-          <input type="email" name='email' placeholder ='Your Email' required/>
-          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+        <form ref={form} onSubmit={sendEmail} method='post'>
+          <input
+            type='text'
+            name='name'
+            placeholder='Your Full Name'
+            required
+          />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea
+            name='message'
+            rows='7'
+            placeholder='Your Message'
+            required
+          ></textarea>
+          <button type='submit' className='btn btn-primary'>
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
