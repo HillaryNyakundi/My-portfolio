b887677e23ea86891d1f89f9bc21e2593156a8cb
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-07-18 13:30:42 +0300

    fix(additions): Added a new project Barcardi Breezer

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,172 +1,172 @@
 import React from "react";
 import "./portfolio.css";
 import ECOMERCE from "../../assets/Ecomerce.png";
-import LAPI6 from "../../assets/Lapi6.webp";
+import LAPI6 from "../../assets/Lapi6.png";
 import PREDICT from "../../assets/Predict.png";
 import INVENT from "../../assets/Invent.png";
 import SORT from "../../assets/Sort.png";
 import LANDING from "../../assets/Landing.png";
 const portfolio = () => {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={ECOMERCE} alt='' />
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={ECOMERCE} alt="" />
           </div>
           <h3>Apparel store with Redux Toolkit for state management</h3>
-          <div className='portfolio__item-cta'>
+          <div className="portfolio__item-cta">
             <a
-              href='https://github.com/HillaryNyakundi/redux-shop'
-              className='btn'
-              target='_blank'
+              href="https://github.com/HillaryNyakundi/redux-shop"
+              className="btn"
+              target="_blank"
             >
               Github
             </a>
-            <a href='' className='btn' target='_blank'>
+            <a href="" className="btn" target="_blank">
               Blog
             </a>
             <a
-              href='https://redux-shop-one-khaki.vercel.app/'
-              className='btn btn-primary'
-              target='_blank'
+              href="https://redux-shop-one-khaki.vercel.app/"
+              className="btn btn-primary"
+              target="_blank"
             >
               Demo
             </a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={INVENT} alt='' />
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={INVENT} alt="" />
           </div>
           <h3>A dynamic inventory Application with CRUD functionalities</h3>
-          <div className='portfolio__item-cta'>
+          <div className="portfolio__item-cta">
             <a
-              href='https://github.com/HillaryNyakundi/my-inventory'
-              className='btn'
-              target='_blank'
+              href="https://github.com/HillaryNyakundi/my-inventory"
+              className="btn"
+              target="_blank"
             >
               Github
             </a>
-            <a href='' className='btn' target='_blank'>
+            <a href="" className="btn" target="_blank">
               Blog
             </a>
             <a
-              href='
-              https://my-inventory-six.vercel.app/'
-              className='btn btn-primary'
-              target='_blank'
+              href="
+              https://my-inventory-six.vercel.app/"
+              className="btn btn-primary"
+              target="_blank"
             >
               Demo
             </a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={PREDICT} alt='Predict' />
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={PREDICT} alt="Predict" />
           </div>
           <h3>
             Final year project, software developers' salary prediction app using
             ML and Python
           </h3>
-          <div className='portfolio__item-cta'>
+          <div className="portfolio__item-cta">
             <a
-              href='https://github.com/HillaryNyakundi/salary_dev'
-              className='btn'
-              target='_blank'
+              href="https://github.com/HillaryNyakundi/salary_dev"
+              className="btn"
+              target="_blank"
             >
               Github
             </a>
-            <a href='https://github.com' className='btn' target='_blank'>
+            <a href="https://github.com" className="btn" target="_blank">
               Blog
             </a>
             <a
-              href='https://developer-salary.streamlit.app/'
-              className='btn btn-primary'
-              target='_blank'
+              href="https://developer-salary.streamlit.app/"
+              className="btn btn-primary"
+              target="_blank"
             >
               Demo
             </a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={LAPI6} alt='' />
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={LAPI6} alt="" />
           </div>
-          <h3>Student API's crud application for backend</h3>
-          <div className='portfolio__item-cta'>
+          <h3>Barcardi Breezer Cocktails</h3>
+          <div className="portfolio__item-cta">
             <a
-              href='https://github.com/HillaryNyakundi/students-API'
-              className='btn'
-              target='_blank'
+              href="https://github.com/HillaryNyakundi/bacardi"
+              className="btn"
+              target="_blank"
             >
               Github
             </a>
-            <a href='' className='btn' target='_blank'>
+            <a href="" className="btn" target="_blank">
               Blog
             </a>
             <a
-              href='http://localhost:3000/api-docs/'
-              className='btn btn-primary'
-              target='_blank'
+              href="https://barcardi.vercel.app/"
+              className="btn btn-primary"
+              target="_blank"
             >
               Demo
             </a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={SORT} alt='' />
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={SORT} alt="" />
           </div>
           <h3>A demonstration of higher order functions in JavaScript</h3>
-          <div className='portfolio__item-cta'>
+          <div className="portfolio__item-cta">
             <a
-              href='https://github.com/HillaryNyakundi/JavaScript-refresher'
-              className='btn'
-              target='_blank'
+              href="https://github.com/HillaryNyakundi/JavaScript-refresher"
+              className="btn"
+              target="_blank"
             >
               Github
             </a>
-            <a href='https://github.com' className='btn' target='_blank'>
+            <a href="https://github.com" className="btn" target="_blank">
               Blog
             </a>
             <a
-              href='https://java-script-refresher.vercel.app/'
-              className='btn btn-primary'
-              target='_blank'
+              href="https://java-script-refresher.vercel.app/"
+              className="btn btn-primary"
+              target="_blank"
             >
               Demo
             </a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={LANDING} alt='' />
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={LANDING} alt="" />
           </div>
           <h3>A landing page for a SACCO business</h3>
-          <div className='portfolio__item-cta'>
+          <div className="portfolio__item-cta">
             <a
-              href='https://github.com/HillaryNyakundi/GuarantorsGuard'
-              className='btn'
-              target='_blank'
+              href="https://github.com/HillaryNyakundi/GuarantorsGuard"
+              className="btn"
+              target="_blank"
             >
               Github
             </a>
-            <a href='https://github.com' className='btn' target='_blank'>
+            <a href="https://github.com" className="btn" target="_blank">
               Blog
             </a>
             <a
-              href='https://guarantorguard.vercel.app/'
-              className='btn btn-primary'
-              target='_blank'
+              href="https://guarantorguard.vercel.app/"
+              className="btn btn-primary"
+              target="_blank"
             >
               Demo
             </a>
