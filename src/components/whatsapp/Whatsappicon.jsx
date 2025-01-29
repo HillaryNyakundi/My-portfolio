927935b7809e5428b6b8fad8fc644869d413fe5b
Hillary Nyakundi <hillarynyakundi66@gmail.com>
2025-01-29 16:22:27 +0300

    minor fixes

--- src/components/whatsapp/Whatsappicon.jsx
+++ src/components/whatsapp/Whatsappicon.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import './Whatsappicon.css'
-import { BsWhatsapp } from 'react-icons/bs'
+import React from "react";
+import "./Whatsappicon.css";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Whatsappicon = () => {
   return (
-    <div className='whatsapp_icon'>
-      <a href="https://wa.me/+254741467228"  target="_blank" rel='noreferrer'>
-        <BsWhatsapp /> 
-        </a>
-        
+    <div className="whatsapp_icon">
+      <a href="https://wa.me/+254741467228" target="_blank" rel="noreferrer">
+        <BsWhatsapp />
+      </a>
     </div>
-  )
-}
+  );
+};
 
 export default Whatsappicon;
